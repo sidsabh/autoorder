773d7b327d23b2423819b13a13f4ebcde1aaec79
mayynag <mayynag@users.noreply.github.com>
2020-07-30 23:34:21 -0500

    ozu ramen menu

--- OTHER/May/sample_menu.py
+++ OTHER/May/sample_menu.py
@@ -11,156 +11,245 @@ menu_collection = db["info"]
 
 
 #Add On Choices
-pizza_small = {
-    "name":"Small",
-    "names_list":["small"],
-    "add_price":0.0
-}
-
-pizza_medium = {
-    "name":"Medium",
-    "names_list":["medium", "regular"],
+tonkotsu = {
+    "name":"Tonkotsu",
+    "names_list":["tonkotsu"],
     "add_price":2.0
 }
 
-pizza_large = {
-    "name":"Large",
-    "names_list":["large"],
-    "add_price":4.0
+tonkotsu_black = {
+    "name":"Tonkotsu black",
+    "names_list":["black"],
+    "add_price":2.0
 }
 
-tomatos = {
-    "name":"Tomatoes",
-    "names_list":["tomato"],
-    "add_price":0.5
-}
-
-lettuce = {
-    "name":"Lettuce",
-    "names_list":["lettuce"],
+miso = {
+    "name":"Miso",
+    "names_list":["miso"],
     "add_price":0.0
 }
 
-onion = {
-    "name":"Onions",
-    "names_list":["onion"],
+spicy_miso = {
+    "name": "Spicy miso",
+    "names_list":["spicy"],
     "add_price":1.0
 }
 
-fries = {
-    "name": "French Fries",
-    "names_list":["fries", "fry"],
-    "add_price":3.0
+shoyu = {
+    "name":"Shoyu",
+    "names_list":["shoyu"],
+    "add_price":0.0
 }
 
-pepperoni = {
-    "name":"Pepperoni",
-    "names_list":["pepperoni"],
-    "add_price":1.75
+shio = {
+    "name":"Shio",
+    "names_list":["shio"],
+    "add_price":0.0
 }
 
-mushroom = {
-    "name":"Mushrooms",
-    "names_list":["mushroom"],
-    "add_price":1.55
+chashu = {
+    "name":"Chashu",
+    "names_list":["chashu", "pork belly"],
+    "add_price":1.0
 }
 
-burger_toppings_none = {
+nitamago = {
+    "name":"Nitamago",
+    "names_list":["nitamago", "boiled egg"],
+    "add_price":1.0
+}
+
+menma = {
+    "name":"Menma",
+    "names_list":["menma", "bamboo"],
+    "add_price":0.50
+}
+
+kikurage = {
+    "name":"Kikurage",
+    "names_list":["kikurage", "mushroom"],
+    "add_price":0.50
+}
+
+narutomaki = {
+    "name":"Narutomaki",
+    "names_list":["narutomaki", "fish cake"],
+    "add_price":0.80
+}
+
+nori_seaweed = {
+    "name":"Nori seaweed",
+    "names_list":["nori seaweed", "nori", "seaweed"],
+    "add_price":0.0
+}
+
+extra_toppings_none = {
     "name":"None",
-    "names_list":["plain", "no topping"],
-    "add_price":0
-}
-
-burger_sides_none = {
-    "name":"None",
-    "names_list":["no sides"],
-    "add_price":0
-}
-
-pizza_toppings_none = {
-    "name":"None",
-    "names_list":["no toppings", "cheese", "plain"],
-    "add_price":0
+    "names_list":["no toppings", "no extra toppings", "plain"],
+    "add_price":0.0
 }
 
 
 #Add On Lists
-burger_toppings = {
-    "name":"Toppings",
-    "choice_list":[onion, lettuce, tomatos, burger_toppings_none],
-    "min_choices":1,
-    "max_choices":100,
-    "prompting_question":"What toppings would you like on your burger, if any?",
-    "sublist_inceptions":[]
-}
-
-burger_sides = {
-    "name":"Sides",
-    "choice_list":[fries, burger_sides_none],
-    "min_choices":1,
-    "max_choices":100,
-    "prompting_question":"What sides would you like with your burger, if any?",
-    "sublist_inceptions":[]
-}
-
-pizza_toppings = {
-    "name":"Toppings",
-    "choice_list":[pepperoni, mushroom, pizza_toppings_none],
-    "min_choices":1,
-    "max_choices":100,
-    "prompting_question":"What toppings would you like on your pizza?",
-    "sublist_inceptions":[]
-}
-
-pizza_sizes = {
-    "name":"Size",
-    "choice_list":[pizza_large, pizza_medium, pizza_small],
+ramen_flavors = {
+    "name":"Flavors",
+    "choice_list":[tonkotsu, tonkotsu_black, miso, spicy_miso, shoyu, shio],
     "min_choices":1,
     "max_choices":1,
-    "prompting_question":"What size pizza would you like?",
+    "prompting_question":"What flavor of ramen would you like?",
     "sublist_inceptions":[]
 }
 
+extra_ramen_toppings = {
+    "name":"Extra toppings",
+    "choice_list":[chashu, nitamago, menma, kikurage, narutomaki, nori_seaweed, extra_toppings_none],
+    "min_choices":1,
+    "max_choices":100,
+    "prompting_question":"Which extra ramen toppings would you like?",
+    "sublist_inceptions":[]
+}
+
+hirata_buns_flavors = {
+    "name":"Ozu Hirata Buns",
+    "choice_list":[bbq_pork_bun, chicken_karaage_bun, wasabi_mini_burger],
+    "min_choices":1,
+    "max_choices":100,
+    "prompting_question":"Which Ozu Hirata Buns would you like?",
+    "sublist_inceptions":[]
+}
 
 #Main Items
-pizza = {
-    "name":"Pizza",
-    "names_list":["pizza", "pie"],
-    "adds_list":[pizza_sizes, pizza_toppings],
+edamame = {
+    "name":"Edamame",
+    "names_list":["edamame"],
+    "adds_list":[],
+    "base_price":2.50
+}
+
+gyoza = {
+    "name":"Gyoza",
+    "names_list":["dumplings", "gyoza"],
+    "adds_list":[],
+    "base_price":2.75
+}
+
+takoyaki = {
+    "name":"Takoyaki",
+    "names_list":["takoyaki"],
+    "adds_list":[],
+    "base_price":3.0
+}
+
+chicken_wing = {
+    "name":"Chicken wing",
+    "names_list":["wings", "chicken wings", "chicken wing"],
+    "adds_list":[],
+    "base_price":3.0
+}
+
+salmon_share = {
+    "name":"Salmon share",
+    "names_list":["salmon share", "salmon"],
+    "adds_list":[],
+    "base_price":6.0
+}
+
+chicken_karaage = {
+    "name":"Chicken karaage",
+    "names_list":["chicken karaage", "karaage"],
+    "adds_list":[],
+    "base_price":4.0
+}
+
+ramen = {
+    "name":"Ramen",
+    "names_list":["ramen", "noodles", "raman"],
+    "adds_list":[ramen_flavors, extra_ramen_toppings],
     "base_price":8.0
 }
 
-burger = {
-    "name":"Hamburger",
-    "names_list":["burger", "hamberder"],
-    "adds_list":[burger_toppings, burger_sides],
+ozu_hirata_buns = {
+    "name":"Ozu Hirata Buns",
+    "names_list":["ozu hirata buns", "buns", "bun", "hirata bun"],
+    "adds_list":[ozu_hirata_buns, burger_sides],
     "base_price":10.0
 }
 
-coke = {
-    "name":"Coke",
-    "names_list":["coke","cola"],
+seared_salmon_plate = {
+    "name":"Seared salmon plate",
+    "names_list":["seared salmon plate", "salmon plate","salmon", "seared salmon"],
     "adds_list":[],
-    "base_price":1.5
+    "base_price":14.0
 }
 
-fries_main = {
-    "name":"French Fries",
-    "names_list":["fries", "fry"],
+chicken_teriyaki = {
+    "name":"Chicken teriyaki",
+    "names_list":["chicken teriyaki", "chicken", "teriyaki", "teriyaki chicken"],
     "adds_list":[],
-    "base_price":5.0
+    "base_price":12.0
+}
+
+japanese_chashu_don = {
+    "name":"Japanese Chashu Don",
+    "names_list":["japanese chashu don", "chashu don", "chashu"],
+    "adds_list":[],
+    "base_price":12.0
+}
+
+short_rib_lover = {
+    "name":"Short rib lover",
+    "names_list":["short rib lover", "short ribs", "short rib"],
+    "adds_list":[],
+    "base_price":13.0
+}
+
+grilled_unagi_bowl = {
+    "name":"Grilled unagi bowl",
+    "names_list":["unagi", "unagi bowl", "grilled unagi bowl"],
+    "adds_list":[],
+    "base_price":14.0
+}
+
+ramune = {
+    "name":"ramune",
+    "names_list":["ramune"],
+    "adds_list":[],
+    "base_price":2.50
+}
+
+coca_cola = {
+    "name":"Coca Cola",
+    "names_list":["coke", "coca"],
+    "adds_list":[],
+    "base_price":1.0
+}
+
+sprite = {
+    "name":"Sprite",
+    "names_list":["sprite"],
+    "adds_list":[],
+    "base_price":1.0
+}
+
+green_tea = {
+    "name":"Green tea",
+    "names_list":["tea", "green tea"],
+    "adds_list":[],
+    "base_price":0.75
 }
 
 #Overall menu
 menu = {
-    "restaurant_name":"Cheesy Does It",
-    "open_intro_message":"Welcome to Cheesy Does It! Here is our menu. What can I get for you?",
-    "closed_intro_message":"Welcome to Cheesy Does It! Here is our menu. We are currently closed but will open soon! Our hours are: [hours]",
+    "restaurant_name":"Ozu Ramen",
+    "open_intro_message":"Welcome to Ozu Ramen! Here is our menu. What can we get for you?",
+    "closed_intro_message":"Welcome to Ozu Ramen! Here is our menu. We are currently closed but will open soon! Our hours are: [hours]",
     "delivery_time":60,
     "pickup_time":30,
     "offers_delivery":True,
     "is_open":True,
-    "main_items":[pizza, burger, coke, fries_main]
+    "main_items":[edamame, gyoza, takoyaki, chicken_wing, salmon_share, chicken_karaage, 
+    ramen, ozu_hirata_buns, seared_salmon_plate, chicken_teriyaki, japanese_chashu_don, short_rib_lover, grilled_unagi_bowl, 
+    ramune, coca_cola, sprite, green_tea]
 }
 
 menu_collection.delete_one({"_id":"menu"})

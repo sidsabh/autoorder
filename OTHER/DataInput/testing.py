fe0629de0c1d6f25017235d13af24cfdbeda3366
isidonnelly <60803783+isidonnelly@users.noreply.github.com>
2020-08-01 14:40:54 -0400

    reworking things

--- OTHER/DataInput/testing.py
+++ OTHER/DataInput/testing.py
@@ -30,7 +30,9 @@ print(session.id)
 '''
 
 cluster = MongoClient("mongodb+srv://admin:54230283752976456@maincluster.ntyoc.mongodb.net/Index?retryWrites=true&w=majority")
-db = cluster["Index"]
+db = cluster["Development"]
 col = db["our_numbers"]
-col.insert_one({"_id":"+14133317017", "codes":["0001","0002"], "index_message":"Welcome to the random restaurant index!"})
+#col.insert_one({"_id":"+14133317017", "codes":["0001","0002"], "index_message":"Welcome to the random restaurant index!"})
+col.insert_one({"_id":"+5155993156","codes":["0000"],"index_message":None})
+
 
--- OTHER/twilio/app.py
+++ OTHER/twilio/app.py
@@ -217,6 +217,76 @@ def checkedout():
         print(restaurant_orders)
         RC.update_one({"_id":correct_order["code"]}, {"$set":{"orders":restaurant_orders}})
 
+    return {}
+
+
+
+#MADE SPECIFICALLY FOR OUR DEMO NUMBER
+@app.route("/checkedoutDEMO", methods=['POST'])
+def checkedoutDEMO():
+
+    print("webhook called!")
+
+    if request.content_length > 1024*1024:
+        print("request too large")
+        abort(400)
+
+    payload = request.get_data()
+    sig_header = request.environ.get('HTTP_STRIPE_SIGNATURE')
+    endpoint_secret = STRIPE_SECRET_ENDPOINT_TEST
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+        payload, sig_header, endpoint_secret
+    )
+
+    except ValueError as e:
+        # Invalid payload
+        print("invalid payload!")
+        return {}, 400
+
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        print("invalid signature!")
+        return {}, 400
+
+    # Handle the checkout.session.completed event
+    if event['type'] == 'checkout.session.completed':
+        print("HI")
+
+        #get the session
+        session = event['data']['object']
+        
+        #get the unique payment intent id
+        pi = session["payment_intent"]
+        
+        #get the order the payment intent corresponds to
+        correct_order = OPC.find_one({"payment_intent":pi})
+
+        #get the restaurant
+        correct_restaurant = RC.find_one({"_id":correct_order["code"]})
+
+        #craft and send a message
+        if correct_order["method_of_getting_food"] == "pickup":
+            resp = "Your order has been processed! Your food will be ready for pickup in about {min} minutes.".format(min=correct_restaurant["pickup_time"])
+        if correct_order["method_of_getting_food"] == "delivery":
+            resp = "Your order has been processed! Your food will be delivered in about {min} minutes.".format(min=correct_restaurant["delivery_time"])   
+        send_message_client(resp, correct_order["from_num"], correct_order["to_num"])
+        
+        #delete the current order
+        OPC.delete_one({"payment_intent":pi})
+
+        #insert order into order collection
+        OC.insert_one(correct_order)
+
+        #input the order id into the restaurant's orders
+        restaurant_orders = correct_restaurant["orders"]
+        print(restaurant_orders)
+        restaurant_orders.append(correct_order["_id"])
+        print(restaurant_orders)
+        RC.update_one({"_id":correct_order["code"]}, {"$set":{"orders":restaurant_orders}})
+
 
 
     return {}

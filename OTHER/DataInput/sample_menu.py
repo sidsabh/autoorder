1158db9edbaea104777b737bbf290034981d9367
isidonnelly <60803783+isidonnelly@users.noreply.github.com>
2020-08-04 15:20:03 -0400

    menu input things

--- OTHER/DataInput/sample_menu.py
+++ OTHER/DataInput/sample_menu.py
@@ -5,64 +5,61 @@ Sample menu in dictionary form.
 
 import pymongo
 from pymongo import MongoClient
-cluster = MongoClient("mongodb+srv://admin:54230283752976456@maincluster.ntyoc.mongodb.net/Index?retryWrites=true&w=majority")
-db = cluster["Index"]
-menu_collection = db["restaurants"]
 
 
 #Add On Choices
 pizza_small = {
     "name":"Small",
     "names_list":["small"],
-    "add_price":0.0
+    "add_price":0
 }
 
 pizza_medium = {
     "name":"Medium",
     "names_list":["medium", "regular"],
-    "add_price":2.0
+    "add_price":200
 }
 
 pizza_large = {
     "name":"Large",
     "names_list":["large"],
-    "add_price":4.0
+    "add_price":400
 }
 
 tomatos = {
     "name":"Tomatoes",
     "names_list":["tomato"],
-    "add_price":0.5
+    "add_price":50
 }
 
 lettuce = {
     "name":"Lettuce",
     "names_list":["lettuce"],
-    "add_price":0.0
+    "add_price":0
 }
 
 onion = {
     "name":"Onions",
     "names_list":["onion"],
-    "add_price":1.0
+    "add_price":100
 }
 
 fries = {
     "name": "French Fries",
     "names_list":["fries", "fry"],
-    "add_price":3.0
+    "add_price":300
 }
 
 pepperoni = {
     "name":"Pepperoni",
     "names_list":["pepperoni"],
-    "add_price":1.75
+    "add_price":175
 }
 
 mushroom = {
     "name":"Mushrooms",
     "names_list":["mushroom"],
-    "add_price":1.55
+    "add_price":155
 }
 
 burger_toppings_none = {
@@ -127,28 +124,28 @@ pizza = {
     "name":"Pizza",
     "names_list":["pizza", "pie"],
     "adds_list":[pizza_sizes, pizza_toppings],
-    "base_price":8.0
+    "base_price":800
 }
 
 burger = {
     "name":"Hamburger",
     "names_list":["burger", "hamberder"],
     "adds_list":[burger_toppings, burger_sides],
-    "base_price":10.0
+    "base_price":1000
 }
 
 coke = {
     "name":"Coke",
     "names_list":["coke","cola", "coca"],
     "adds_list":[],
-    "base_price":1.5
+    "base_price":150
 }
 
 fries_main = {
     "name":"French Fries",
     "names_list":["fries", "fry"],
     "adds_list":[],
-    "base_price":5.0
+    "base_price":50
 }
 '''
 #Overall menu
@@ -167,33 +164,57 @@ menu_collection.insert_one({
     "orders":[]
 })
 '''
+
+cluster = MongoClient("mongodb+srv://admin:54230283752976456@maincluster.ntyoc.mongodb.net/Index?retryWrites=true&w=majority")
+db = cluster["Deployment"]
+menu_collection = db["restaurants"]
+
 menu_collection.insert_one({
-    "_id":"0001",
-    "name":"Testaurant",
-    "names":["test"],
+    "_id":"fries@gmail.com",
+    "name":"50 Cent Friesaurant",
+    "names":["fries"],
     "main_items": [pizza, burger, coke, fries_main],
-    "link":"https://iili.io/dIAGcl.jpg",
-    "open_intro":"Welcome to the Testaurant! Here is our menu. What can I get for you?",
-    "closed_intro":"Welcome to the Testaurant! Here is our menu. We are currently closed but will open soon! Our hours are: [hours]",
+    "link":"https://iili.io/dyBrEG.jpg",
+    "open_intro":"Welcome to the Testaurant!",
+    "closed_intro":"Welcome to the Testaurant! We are currently closed but will open soon! Our hours are: [hours]",
     "delivery_time":60,
     "pickup_time":30,
     "offers_delivery":True,
-    "is_open":False,
-    "orders":[]
+    "is_open":True,
+    "orders":[],
+    "tax":0.06
 })
-
+"""
 menu_collection.insert_one({
-    "_id":"0002",
-    "name":"Other Restaurant",
-    "names":["other"],
+    "_id":"cheesydoesit@gmail.com",
+    "name":"Cheesy Does It",
+    "names":["cheesy"],
     "main_items": [pizza, burger, coke, fries_main],
-    "link":"https://iili.io/dIAGcl.jpg",
-    "open_intro":"Welcome to the Other Restaurant! Here is our menu. What can I get for you?",
-    "closed_intro":"Welcome to the Other Restaurant! Here is our menu. We are currently closed but will open soon! Our hours are: [hours]",
+    "link":"https://iili.io/dyBrEG.jpg",
+    "open_intro":"Welcome to Cheesy Does It!",
+    "closed_intro":"Welcome to Cheesy Does It! We are currently closed but will open soon! Our hours are: [hours]",
     "delivery_time":60,
     "pickup_time":30,
     "offers_delivery":False,
     "is_open":True,
-    "orders":[]
+    "orders":[],
+    "tax":0.06
 })
 
+menu_collection.insert_one({
+    "_id":"otherrestaurant@gmail.com",
+    "name":"Other Restaurant",
+    "names":["other"],
+    "main_items": [pizza, burger, coke, fries_main],
+    "link":"https://iili.io/dyBrEG.jpg",
+    "open_intro":"Welcome to the Other Restaurant!",
+    "closed_intro":"Welcome to the Other Restaurant! We are currently closed but will open soon! Our hours are: [hours]",
+    "delivery_time":60,
+    "pickup_time":30,
+    "offers_delivery":False,
+    "is_open":False,
+    "orders":[],
+    "tax":0.06
+})
+
+"""
--- OTHER/May/sample_menu.py
+++ OTHER/May/sample_menu.py
@@ -14,55 +14,55 @@ collection = db["restaurants"]
 chashu = {
     "name":"Chashu",
     "names_list":["chashu", "pork belly"],
-    "add_price":1.0
+    "add_price":100
 }
 
 nitamago = {
     "name":"Nitamago",
     "names_list":["nitamago", "boiled egg"],
-    "add_price":1.0
+    "add_price":100
 }
 
 menma = {
     "name":"Menma",
     "names_list":["menma", "bamboo"],
-    "add_price":0.50
+    "add_price":50
 }
 
 kikurage = {
     "name":"Kikurage",
     "names_list":["kikurage", "mushroom"],
-    "add_price":0.50
+    "add_price":50
 }
 
 narutomaki = {
     "name":"Narutomaki",
     "names_list":["narutomaki", "fish cake"],
-    "add_price":0.80
+    "add_price":80
 }
 
 nori_seaweed = {
     "name":"Nori Seaweed",
     "names_list":["nori", "seaweed"],
-    "add_price":0.0
+    "add_price":0
 }
 
 butter = {
     "name":"Butter",
     "names_list":["butter"],
-    "add_price":0.0
+    "add_price":0
 }
 
 corn = {
     "name":"Corn",
     "names_list":["corn"],
-    "add_price":0.0
+    "add_price":0
 }
 
 extra_toppings_none = {
     "name":"None",
     "names_list":["no extra toppings", "plain"],
-    "add_price":0.0
+    "add_price":0
 }
 
 #Add On Lists
@@ -80,173 +80,173 @@ edamame = {
     "name":"Edamame",
     "names_list":["edamame"],
     "adds_list":[],
-    "base_price":2.50
+    "base_price":250
 }
 
 gyoza = {
     "name":"Gyoza",
     "names_list":["dumplings", "gyoza"],
     "adds_list":[],
-    "base_price":2.75
+    "base_price":275
 }
 
 takoyaki = {
     "name":"Takoyaki",
     "names_list":["takoyaki"],
     "adds_list":[],
-    "base_price":3.0
+    "base_price":300
 }
 
 chicken_wing = {
     "name":"Chicken Wing",
     "names_list":["wings"],
     "adds_list":[],
-    "base_price":3.0
+    "base_price":300
 }
 
 salmon_share = {
     "name":"Salmon Share",
     "names_list":["salmon share"],
     "adds_list":[],
-    "base_price":6.0
+    "base_price":600
 }
 
 chicken_karaage = {
     "name":"Chicken Karaage",
     "names_list":["karaage"],
     "adds_list":[],
-    "base_price":4.0
+    "base_price":400
 }
 
 tonkotsu_ramen = {
     "name":"Tonkotsu Ramen",
     "names_list":["tonkotsu"],
     "adds_list":[extra_ramen_toppings],
-    "base_price":12.0
+    "base_price":1200
 }
 
 tonkotsu_black_ramen = {
     "name":"Tonkotsu Black Ramen",
     "names_list":["tonkotsu black", "black tonkotsu"],
     "adds_list":[extra_ramen_toppings],
-    "base_price":12.0
+    "base_price":1200
 }
 
 miso_ramen = {
     "name":"Miso Ramen",
     "names_list":["miso"],
     "adds_list":[extra_ramen_toppings],
-    "base_price":10.0
+    "base_price":1000
 }
 
 spicy_miso_ramen = {
     "name":"Spicy Miso Ramen",
     "names_list":["spicy miso", "miso spicy"],
     "adds_list":[extra_ramen_toppings],
-    "base_price":10.0
+    "base_price":1000
 }
 
 shoyu_ramen = {
     "name":"Shoyu Ramen",
     "names_list":["shoyu"],
     "adds_list":[extra_ramen_toppings],
-    "base_price":11.0
+    "base_price":1100
 }
 
 shio_ramen = {
     "name":"Shio Ramen",
     "names_list":["shio"],
     "adds_list":[extra_ramen_toppings],
-    "base_price":11.0
+    "base_price":1100
 }
 
 bbq_pork_bun = {
     "name":"BBQ Pork Bun",
     "names_list":["bbq pork"],
     "adds_list":[],
-    "base_price":3.0
+    "base_price":300
 }
 
 chicken_karaage_bun = {
     "name":"Chicken Karaage Bun",
     "names_list":["chicken karaage"],
     "adds_list":[],
-    "base_price":3.0
+    "base_price":300
 }
 
 wasabi_mini_burger = {
     "name":"Wasabi Mini Burger",
     "names_list":["wasabi mini burger"],
     "adds_list":[],
-    "base_price":3.0
+    "base_price":300
 }
 
 seared_salmon_plate = {
     "name":"Seared Salmon Plate",
     "names_list":["seared salmon", "salmon plate"],
     "adds_list":[],
-    "base_price":14.0
+    "base_price":1400
 }
 
 chicken_teriyaki = {
     "name":"Chicken Teriyaki",
     "names_list":["chicken", "teriyaki"],
     "adds_list":[],
-    "base_price":12.0
+    "base_price":1200
 }
 
 japanese_chashu_don = {
     "name":"Japanese Chashu Don",
     "names_list":["chashu"],
     "adds_list":[],
-    "base_price":12.0
+    "base_price":1200
 }
 
 short_rib_lover = {
     "name":"Short Rib Lover",
     "names_list":["short rib"],
     "adds_list":[],
-    "base_price":13.0
+    "base_price":1300
 }
 
 grilled_unagi_bowl = {
     "name":"Grilled Unagi Bowl",
     "names_list":["unagi"],
     "adds_list":[],
-    "base_price":14.0
+    "base_price":1400
 }
 
 ramune = {
     "name":"Ramune",
     "names_list":["ramune"],
     "adds_list":[],
-    "base_price":2.50
+    "base_price":250
 }
 
 coca_cola = {
     "name":"Coca Cola",
     "names_list":["coke", "coca", "cola"],
     "adds_list":[],
-    "base_price":1.0
+    "base_price":100
 }
 
 sprite = {
     "name":"Sprite",
     "names_list":["sprite"],
     "adds_list":[],
-    "base_price":1.0
+    "base_price":100
 }
 
 green_tea = {
     "name":"Green Tea",
     "names_list":["tea"],
     "adds_list":[],
-    "base_price":0.75
+    "base_price":75
 }
 
 #Overall menu
 menu = {
-    "_id":"0003",
+    "_id":"ozuramen@gmail.com",
     "name":"Ozu Ramen",
     "names":["ozu", "ramen"],
     "main_items":[edamame, gyoza, takoyaki, chicken_wing, salmon_share, chicken_karaage, 
@@ -260,8 +260,13 @@ menu = {
     "pickup_time":30,
     "offers_delivery":True,
     "is_open":True,
-    "orders":[]
+    "orders":[],
+    "tax":0.06
 }
 
+
+cluster = MongoClient("mongodb+srv://admin:54230283752976456@maincluster.ntyoc.mongodb.net/Index?retryWrites=true&w=majority")
+db = cluster["Deployment"]
+collection = db["restaurants"]
 
 collection.insert_one(menu)
